071090166f2f0e5a0a12d137eefc00fd738b738e
宇阳 <liuyuyang1024@yeah.net>
2024-12-18 19:01:32 +0800

    1

--- src/controller/ArticleController.ts
+++ src/controller/ArticleController.ts
@@ -1,9 +1,54 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { ArticleService } from '@/service/ArticleService';
+import { Result } from '@/utils';
+import { Article } from '@/data/types';
 
-@Controller("/article")
+@Controller('article')
 export class ArticleController {
-  @Get()
-  list() {
-    return 'Hello ThriveX';
+  constructor(private articleService: ArticleService) {}
+
+  @Post()
+  async add(@Body() article: Article) {
+    const data = await this.articleService.add(article);
+    return Result.success(data);
+  }
+
+  @Delete('batch')
+  async delBatch(@Body() ids: number[]) {
+    await this.articleService.delBatch(ids);
+    return Result.success();
+  }
+
+  @Delete(':id')
+  async del(@Param('id') id: number) {
+    await this.articleService.del(id);
+    return Result.success();
+  }
+
+  @Patch()
+  async edit(@Body() article: Article) {
+    await this.articleService.edit(article);
+    return Result.success();
+  }
+
+  @Get(':id')
+  async get(@Param('id') id: number) {
+    const data = await this.articleService.get(id);
+    return Result.success(data);
+  }
+
+  @Post('list')
+  async list() {
+    const list = await this.articleService.list();
+    return Result.success(list);
+  }
+
+  @Post('paging')
+  async paging(
+    @Query('page') page: number,
+    @Query('size') size: number
+  ) {
+    const data = await this.articleService.paging(page, size);
+    return Result.success(data);
   }
 }
--- src/service/ArticleService.ts
+++ src/service/ArticleService.ts
@@ -1,6 +1,96 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './PrismaService';
+import { Article } from '@/data/types';
 
 @Injectable()
 export class ArticleService {
+    constructor(private prisma: PrismaService) { }
 
+    async add(article: Article) {
+        return await this.prisma.article.create({
+            data: {
+                title: article.title,
+                description: article.description,
+                content: article.content,
+                cover: article.cover,
+                tag_ids: article.tag_ids,
+                is_draft: article.is_draft || 0,
+                is_del: 0,
+                create_time: new Date().toISOString(),
+                view: 0,
+                comment: 0
+            }
+        });
+    }
+
+    async del(id: number) {
+        // 软删除
+        await this.prisma.article.update({
+            where: { id },
+            data: { is_del: 1 }
+        });
+    }
+
+    async delBatch(ids: number[]) {
+        // 批量软删除
+        await this.prisma.article.updateMany({
+            where: {
+                id: { in: ids }
+            },
+            data: { is_del: 1 }
+        });
+    }
+
+    async edit(article: Article) {
+        const { id, ...data } = article;
+        await this.prisma.article.update({
+            where: { id },
+            data
+        });
+    }
+
+    async get(id: number) {
+        return await this.prisma.article.findFirst({
+            where: {
+                id,
+                is_del: 0
+            }
+        });
+    }
+
+    async list() {
+        return await this.prisma.article.findMany({
+            where: {
+                is_del: 0,
+                is_draft: 0
+            },
+
+            orderBy: { create_time: 'desc' }
+        });
+    }
+
+    async paging(page: number, size: number) {
+        const where = {
+            is_del: 0,
+            is_draft: 0,
+        };
+
+        const [list, total] = await Promise.all([
+            this.prisma.article.findMany({
+                where,
+                skip: (page - 1) * size,
+                take: size,
+                orderBy: { create_time: 'desc' }
+            }),
+
+            this.prisma.article.count({ where })
+        ]);
+
+        return {
+            list,
+            page,
+            size,
+            total
+        };
+    }
 }
